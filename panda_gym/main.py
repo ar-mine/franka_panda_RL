b89ef611a1033e60a4d144c37a90bc98c172f580
ar-mine <934218777@qq.com>
2022-02-18 09:46:13 +0800

    RL part init
    No Ros interface implemented

---
+++ panda_gym/main.py
@@ -0,0 +1,17 @@
+import gym
+import panda_gym
+
+env = gym.make('PandaReachDense-v2', render=True)
+
+obs = env.reset()
+done = False
+for i in range(100000):
+    action = env.action_space.sample() # random action
+    obs, reward, done, info = env.step(action)
+    print("======>observation<======", obs['observation'])
+    print("------>achieved_goal<------", obs['achieved_goal'])
+    print("******>desired_goal<******", obs['desired_goal'])
+    print("++++++>reward<++++++", reward)
+    print(">>>done<<<", done, ">>>action<<<", action)
+
+env.close()
