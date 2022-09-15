2a7c521d876ed4f4b02909b864e380946d83331a
ar-mine <934218777@qq.com>
2022-09-15 13:37:02 +0800

    feat: Create ros2_control based controllers package and init position_servo controllers(No implement)

---
+++ am_franka_controllers/include/am_franka_controllers/position_servo.h
@@ -0,0 +1,33 @@
+//
+// Created by armine on 9/15/22.
+//
+
+#ifndef BUILD_POSITION_SERVO_H
+#define BUILD_POSITION_SERVO_H
+
+#include <string>
+
+#include <controller_interface/controller_interface.hpp>
+#include <rclcpp/duration.hpp>
+#include <rclcpp/time.hpp>
+
+namespace am_franka_controllers{
+
+    class PositionServoController : public controller_interface::ControllerInterface {
+        public:
+        controller_interface::return_type init(const std::string& controller_name) override;
+        controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+        controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+        CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+        CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+        CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
+        controller_interface::return_type update() override;
+
+        private:
+        std::string arm_id_;
+        const int num_joints = 7;
+    };
+
+}
+
+#endif //BUILD_POSITION_SERVO_H
---
+++ am_franka_controllers/src/realtime_servo/position_servo.cpp
@@ -0,0 +1,94 @@
+//
+// Created by armine on 9/15/22.
+//
+
+#include "am_franka_controllers/position_servo.h"
+
+namespace am_franka_controllers{
+    controller_interface::return_type PositionServoController::init(const std::string& controller_name)
+    {
+        // best place to initialize the variables, reserve memory,
+        // and most importantly, declare node parameters used by the controller.
+        auto ret = ControllerInterface::init(controller_name);
+        if (ret != controller_interface::return_type::OK) {
+            return ret;
+        }
+
+        try {
+            auto_declare<std::string>("arm_id", "panda");
+        } catch (const std::exception& e) {
+            fprintf(stderr, "Exception thrown during init stage with message: %s \n", e.what());
+            return controller_interface::return_type::ERROR;
+        }
+        return controller_interface::return_type::OK;
+    }
+
+    controller_interface::InterfaceConfiguration PositionServoController::command_interface_configuration() const
+    {
+        /**
+         *  ALL and NONE option will ask for access to all available interfaces or none of them.
+         *  The INDIVIDUAL configuration needs a detailed list of required interface names.
+         * */
+        controller_interface::InterfaceConfiguration config;
+        config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+
+        for (int i = 1; i <= num_joints; ++i) {
+            config.names.push_back(arm_id_ + "_joint" + std::to_string(i) + "/effort");
+        }
+        return config;
+    }
+
+    controller_interface::InterfaceConfiguration PositionServoController::state_interface_configuration() const
+    {
+        return {};
+    }
+
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+    PositionServoController::on_configure(const rclcpp_lifecycle::State& previous_state)
+    {
+        // Parameters are usually read here, and everything is prepared so that the controller can be started.
+        arm_id_ = node_->get_parameter("arm_id").as_string();
+        return CallbackReturn::SUCCESS;
+    }
+
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+    PositionServoController::on_activate(const rclcpp_lifecycle::State& previous_state)
+    {
+        /**
+         * Implement the on_activate method with checking, and potentially sorting,
+         * the interfaces and assigning members’ initial values.
+         * This method is part of the real-time loop, therefore avoid any reservation of memory and,
+         * in general, keep it as short as possible.
+         * */
+        return CallbackReturn::SUCCESS;
+    }
+
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+    PositionServoController::on_deactivate(const rclcpp_lifecycle::State& previous_state)
+    {
+        /**
+         * Implement the on_deactivate method, which does the opposite of on_activate.
+         * In many cases, this method is empty.
+         * This method should also be real-time safe as much as possible.*/
+        return CallbackReturn::SUCCESS;
+    }
+
+    controller_interface::return_type PositionServoController::update()
+    {
+        /**
+         * Implement the update method as the main entry point.
+         * The method should be implemented with real-time constraints in mind.
+         * When this method is called, the state interfaces have the most recent values from the hardware,
+         * and new commands for the hardware should be written into command interfaces.
+         * */
+        for (auto& command_interface : command_interfaces_) {
+            command_interface.set_value(0);
+        }
+        return controller_interface::return_type::OK;
+    }
+}
+
+#include "pluginlib/class_list_macros.hpp"
+// NOLINTNEXTLINE
+PLUGINLIB_EXPORT_CLASS(am_franka_controllers::PositionServoController,
+        controller_interface::ControllerInterface)
