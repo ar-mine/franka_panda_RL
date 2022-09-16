c0e95b459f0ca84d4efc7f9d68b9b72efb79a2fc
ar-mine <934218777@qq.com>
2022-09-16 19:45:11 +0800

    feat: Use KDL to get realtime  FW pose

--- am_franka_controllers/include/am_franka_controllers/position_servo.h
+++ am_franka_controllers/include/am_franka_controllers/position_servo.h
@@ -6,7 +6,14 @@
 #define BUILD_POSITION_SERVO_H
 
 #include <string>
+#include <Eigen/Eigen>
+#include <boost/scoped_ptr.hpp>
+
 #include <kdl_parser/kdl_parser.hpp>
+#include <kdl/chain.hpp>
+#include <kdl/chainfksolver.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/frames_io.hpp>
 
 #include <controller_interface/controller_interface.hpp>
 #include <rclcpp/duration.hpp>
@@ -16,6 +23,10 @@ namespace am_franka_controllers{
 
     class PositionServoController : public controller_interface::ControllerInterface {
         public:
+        using Vector3d = Eigen::Matrix<double, 3, 1>;
+        using Vector4d = Eigen::Matrix<double, 4, 1>;
+        using Vector7d = Eigen::Matrix<double, 7, 1>;
+
         controller_interface::return_type init(const std::string& controller_name) override;
         controller_interface::InterfaceConfiguration command_interface_configuration() const override;
         controller_interface::InterfaceConfiguration state_interface_configuration() const override;
@@ -23,12 +34,24 @@ namespace am_franka_controllers{
         CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
         CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
         controller_interface::return_type update() override;
+        void updateJointStates();
+        void updateFKStates();
 
         private:
         std::string arm_id_;
         const int num_joints = 7;
-        KDL::Tree robot_tree;
-        KDL::Chain robot_chain;
+        Vector7d q_;
+        Vector7d q_goal_;
+        Vector7d dq_;
+        Vector7d dq_filtered_;
+        Vector7d eef_;
+        Vector3d eef_t_;
+        Vector4d eef_r_;
+
+        KDL::Tree robot_tree_;
+        KDL::Chain robot_chain_;
+        KDL::JntArray jnt_pos_, jnt_effort_;
+        boost::scoped_ptr<KDL::ChainFkSolverPos> jnt_to_pose_solver_;
     };
 
 }
--- am_franka_controllers/src/realtime_servo/position_servo.cpp
+++ am_franka_controllers/src/realtime_servo/position_servo.cpp
@@ -3,6 +3,8 @@
 //
 #include "am_franka_controllers/position_servo.h"
 
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("am_franka_controllers/position_servo.cpp");
+
 namespace am_franka_controllers{
     controller_interface::return_type PositionServoController::init(const std::string& controller_name)
     {
@@ -39,7 +41,13 @@ namespace am_franka_controllers{
 
     controller_interface::InterfaceConfiguration PositionServoController::state_interface_configuration() const
     {
-        return {};
+        controller_interface::InterfaceConfiguration config;
+        config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+        for (int i = 1; i <= num_joints; ++i) {
+            config.names.push_back(arm_id_ + "_joint" + std::to_string(i) + "/position");
+            config.names.push_back(arm_id_ + "_joint" + std::to_string(i) + "/velocity");
+        }
+        return config;
     }
 
     rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
@@ -48,12 +56,17 @@ namespace am_franka_controllers{
         // Parameters are usually read here, and everything is prepared so that the controller can be started.
         std::string robot_desc_string;
         robot_desc_string = node_->get_parameter("robot_description").as_string();
-        if (!kdl_parser::treeFromString(robot_desc_string, robot_tree)){
+        if (!kdl_parser::treeFromString(robot_desc_string, robot_tree_)){
             fprintf(stderr, "Failed to construct kdl tree\n");
             return CallbackReturn::FAILURE;
         }
 
-        robot_tree.getChain("panda_link0", "panda_hand_tcp", robot_chain);
+        robot_tree_.getChain("panda_link0", "panda_hand_tcp", robot_chain_);
+        // Create solver based on kinematic chain
+        jnt_to_pose_solver_.reset(new KDL::ChainFkSolverPos_recursive(robot_chain_));
+        // resizes the joint state vectors in non-realtime
+        jnt_pos_.resize(robot_chain_.getNrOfJoints());
+//        RCLCPP_INFO(LOGGER, "Total number of joints: %d", robot_chain_.getNrOfJoints());
 
         arm_id_ = node_->get_parameter("arm_id").as_string();
         return CallbackReturn::SUCCESS;
@@ -68,6 +81,9 @@ namespace am_franka_controllers{
          * This method is part of the real-time loop, therefore avoid any reservation of memory and,
          * in general, keep it as short as possible.
          * */
+        RCLCPP_INFO(LOGGER, "Now running on_activate.");
+        updateJointStates();
+
         return CallbackReturn::SUCCESS;
     }
 
@@ -89,10 +105,47 @@ namespace am_franka_controllers{
          * When this method is called, the state interfaces have the most recent values from the hardware,
          * and new commands for the hardware should be written into command interfaces.
          * */
+//        RCLCPP_INFO(LOGGER, "Now running update.");
+        updateJointStates();
+        updateFKStates();
+
         for (auto& command_interface : command_interfaces_) {
             command_interface.set_value(0);
         }
         return controller_interface::return_type::OK;
+    }
+
+    void PositionServoController::updateJointStates() {
+        for (auto i = 0; i < num_joints; ++i) {
+            const auto& position_interface = state_interfaces_.at(2 * i);
+            const auto& velocity_interface = state_interfaces_.at(2 * i + 1);
+
+            assert(position_interface.get_interface_name() == "position");
+            assert(velocity_interface.get_interface_name() == "velocity");
+
+            q_(i) = position_interface.get_value();
+            dq_(i) = velocity_interface.get_value();
+//            RCLCPP_INFO(LOGGER, "Current q: %.2f %.2f %.2f %.2f %.2f %.2f %.2f",
+//                       q_[0], q_[1], q_[2], q_[3], q_[4], q_[5], q_[6]);
+        }
+    }
+
+    void PositionServoController::updateFKStates()
+    {
+        // Assign some values to the joint positions
+        for(unsigned int i=0;i<7;i++){
+            jnt_pos_(i) = q_[i];
+        }
+        // computes Cartesian pose in realtime
+        KDL::Frame current_pose;
+        jnt_to_pose_solver_->JntToCart(jnt_pos_, current_pose);
+
+        eef_t_ = {current_pose.p.data[0], current_pose.p.data[1], current_pose.p.data[2]};
+        current_pose.M.GetQuaternion(eef_r_[0], eef_r_[1], eef_r_[2], eef_r_[3]);
+//        RCLCPP_INFO(LOGGER, "EEF translation: %.2f %.2f %.2f, rotation: %.2f %.2f %.2f %.2f",
+//                eef_t_[0], eef_t_[1], eef_t_[2],
+//                eef_r_[0], eef_r_[1], eef_r_[2], eef_r_[3]);
+
     }
 }
 
