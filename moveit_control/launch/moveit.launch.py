73a5ff8759e11740b916321c9e61f82af25e914b
armine <934218777@qq.com>
2022-12-12 17:40:00 +0800

    Add controller interface based on MoveitCpp

---
+++ moveit_control/launch/moveit.launch.py
@@ -0,0 +1,232 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription,
+                            Shutdown)
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import yaml
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def generate_launch_description():
+    robot_ip_parameter_name = 'robot_ip'
+    use_fake_hardware_parameter_name = 'use_fake_hardware'
+    fake_sensor_commands_parameter_name = 'fake_sensor_commands'
+    hand_parameter_name = 'hand'
+
+    robot_ip = LaunchConfiguration(robot_ip_parameter_name)
+    use_fake_hardware = LaunchConfiguration(use_fake_hardware_parameter_name)
+    fake_sensor_commands = LaunchConfiguration(fake_sensor_commands_parameter_name)
+    hand = LaunchConfiguration(hand_parameter_name)
+
+    # Declaration
+    robot_arg = DeclareLaunchArgument(
+        robot_ip_parameter_name,
+        description='Hostname or IP address of the robot.')
+    use_fake_hardware_arg = DeclareLaunchArgument(
+        use_fake_hardware_parameter_name,
+        default_value='false',
+        description='Use fake hardware')
+    fake_sensor_commands_arg = DeclareLaunchArgument(
+        fake_sensor_commands_parameter_name,
+        default_value='false',
+        description="Fake sensor commands. Only valid when '{}' is true".format(
+            use_fake_hardware_parameter_name))
+    hand_arg = DeclareLaunchArgument(
+        hand_parameter_name,
+        default_value='false',
+        description="Whether use load gripper model.")
+    # gripper_launch_file = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([PathJoinSubstitution(
+    #         [FindPackageShare('franka_gripper'), 'launch', 'gripper.launch.py'])]),
+    #     launch_arguments={'robot_ip': robot_ip,
+    #                       use_fake_hardware_parameter_name: use_fake_hardware}.items(),
+    # )
+    moveit_cpp_yaml_file_name = (
+            get_package_share_directory("moveit_control") + "/config/moveit_cpp.yaml"
+    )
+
+    # planning_context
+    franka_xacro_file = os.path.join(get_package_share_directory('franka_description'), 'robots',
+                                     'panda_arm.urdf.xacro')
+    robot_description_config = Command(
+        [FindExecutable(name='xacro'), ' ', franka_xacro_file, ' hand:=', hand,
+         ' robot_ip:=', robot_ip, ' use_fake_hardware:=', use_fake_hardware,
+         ' fake_sensor_commands:=', fake_sensor_commands])
+
+    robot_description = {'robot_description': robot_description_config}
+
+    franka_semantic_xacro_file = os.path.join(get_package_share_directory('franka_moveit_config'),
+                                              'srdf',
+                                              'panda_arm.srdf.xacro')
+    robot_description_semantic_config = Command(
+        [FindExecutable(name='xacro'), ' ', franka_semantic_xacro_file, ' hand:=', hand]
+    )
+    robot_description_semantic = {
+        'robot_description_semantic': robot_description_semantic_config
+    }
+
+    kinematics_yaml = load_yaml(
+        'franka_moveit_config', 'config/kinematics.yaml'
+    )
+
+    # Planning Functionality
+    ompl_planning_pipeline_config = {
+        'ompl': {
+            'planning_plugin': 'ompl_interface/OMPLPlanner',
+            'request_adapters': 'default_planner_request_adapters/AddTimeOptimalParameterization '
+                                'default_planner_request_adapters/ResolveConstraintFrames '
+                                'default_planner_request_adapters/FixWorkspaceBounds '
+                                'default_planner_request_adapters/FixStartStateBounds '
+                                'default_planner_request_adapters/FixStartStateCollision '
+                                'default_planner_request_adapters/FixStartStatePathConstraints',
+            'start_state_max_bounds_error': 0.1,
+        }
+    }
+    ompl_planning_yaml = load_yaml(
+        'franka_moveit_config', 'config/ompl_planning.yaml'
+    )
+    ompl_planning_pipeline_config['ompl'].update(ompl_planning_yaml)
+
+    # Trajectory Execution Functionality
+    # moveit_simple_controllers_yaml = load_yaml(
+    #     'franka_moveit_config', 'config/panda_controllers.yaml'
+    # )
+    moveit_simple_controllers_yaml = load_yaml(
+        'franka_moveit_config', 'config/panda_controllers_no_hand.yaml'
+    )
+    moveit_controllers = {
+        'moveit_simple_controller_manager': moveit_simple_controllers_yaml,
+        'moveit_controller_manager': 'moveit_simple_controller_manager'
+                                     '/MoveItSimpleControllerManager',
+    }
+
+    trajectory_execution = {
+        'moveit_manage_controllers': True,
+        'trajectory_execution.allowed_execution_duration_scaling': 1.2,
+        'trajectory_execution.allowed_goal_duration_margin': 0.5,
+        'trajectory_execution.allowed_start_tolerance': 0.01,
+    }
+
+    planning_scene_monitor_parameters = {
+        'publish_planning_scene': True,
+        'publish_geometry_updates': True,
+        'publish_state_updates': True,
+        'publish_transforms_updates': True,
+    }
+
+    # MoveItCpp demo executable
+    moveit_cpp_node = Node(
+        name="moveit_cpp_tutorial",
+        package="moveit_control",
+        executable="moveit_cpp_tutorial",
+        output="screen",
+        parameters=[
+            moveit_cpp_yaml_file_name,
+            robot_description,
+            robot_description_semantic,
+            kinematics_yaml,
+            ompl_planning_pipeline_config,
+            moveit_controllers,
+        ],
+    )
+
+    rviz_base = os.path.join(get_package_share_directory('moveit_control'), 'rviz')
+    rviz_full_config = os.path.join(rviz_base, 'moveit.rviz')
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='log',
+        arguments=['-d', rviz_full_config],
+        parameters=[
+            robot_description,
+            robot_description_semantic,
+            # ompl_planning_pipeline_config,
+        ],
+    )
+
+    # Static TF
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "panda_link0"],
+    )
+
+    # Publish TF
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='both',
+        parameters=[robot_description],
+    )
+
+    ros2_controllers_path = os.path.join(
+        get_package_share_directory('franka_moveit_config'),
+        'config',
+        'panda_ros_controllers.yaml',
+    )
+    ros2_control_node = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[robot_description, ros2_controllers_path],
+        remappings=[('joint_states', 'franka/joint_states')],
+        output={
+            'stdout': 'screen',
+            'stderr': 'screen',
+        },
+        on_exit=Shutdown(),
+    )
+
+    # Load controllers
+    load_controllers = []
+    for controller in ['panda_arm_controller', 'joint_state_broadcaster']:
+        load_controllers += [
+            ExecuteProcess(
+                cmd=['ros2 run controller_manager spawner.py {}'.format(controller)],
+                shell=True,
+                output='screen',
+            )
+        ]
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[
+            {'source_list': ['franka/joint_states'], 'rate': 30}],
+    )
+
+    return LaunchDescription(
+        [robot_arg,
+         use_fake_hardware_arg,
+         fake_sensor_commands_arg,
+         hand_arg,
+         moveit_cpp_node,
+         rviz_node,
+         static_tf,
+         robot_state_publisher,
+         ros2_control_node,
+         joint_state_publisher,
+         ]
+        + load_controllers
+    )
